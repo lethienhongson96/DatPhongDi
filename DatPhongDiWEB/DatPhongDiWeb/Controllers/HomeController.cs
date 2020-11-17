3efca18ea4d679698ebe462b95b17fc4ea884788
Phuc Vui <phucvui16495@gmail.com>
2020-11-17 10:25:43 +0700

    create Vui-dev

--- DatPhongDiWEB/DatPhongDiWeb/Controllers/HomeController.cs
+++ DatPhongDiWEB/DatPhongDiWeb/Controllers/HomeController.cs
@@ -33,5 +33,11 @@ namespace DatPhongDiWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public HomeController vui()
+        {
+            return 1;
+        }
     }
 }
