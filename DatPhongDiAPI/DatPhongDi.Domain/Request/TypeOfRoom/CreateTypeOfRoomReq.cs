9e1bddba5f2e515a3c15dc6b3ad927232cd4ddd6
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-11-17 14:55:58 +0700

    HongSon-->Delete class default when create project and create foder request, response

---
+++ DatPhongDiAPI/DatPhongDi.Domain/Request/TypeOfRoom/CreateTypeOfRoomReq.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatPhongDi.Domain.Request.TypeOfRoom
+{
+    public class CreateTypeOfRoomReq
+    {
+        public string Name { get; set; }
+        public int Status{ get; set; }
+    }
+}
---
+++ DatPhongDiAPI/DatPhongDi.Domain/Response/TypeOfRoom/CreateTypeOfRoomRes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatPhongDi.Domain.Response.TypeOfRoom
+{
+    public class CreateTypeOfRoomRes
+    {
+        public int TypeOfRoomId { get; set; }
+        public string Message { get; set; }
+    }
+}
