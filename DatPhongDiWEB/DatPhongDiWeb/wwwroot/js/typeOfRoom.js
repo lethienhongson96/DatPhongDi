2ce8729a47930b075e79acfcf8b9b52d568a9d9c
ADMIN <ADMIN@KHOA>
2020-11-24 22:31:31 +0700

    khoa-dev completed type of room
    hoàn thành xong CRUD type of room trên giao diện admin, sửa lỗi các issue trong sql

--- DatPhongDiWEB/DatPhongDiWeb/wwwroot/js/typeOfRoom.js
+++ DatPhongDiWEB/DatPhongDiWeb/wwwroot/js/typeOfRoom.js
@@ -15,8 +15,8 @@ typeOfRoom.showData = function () {
                         <td>${v.statusName}</td>
                         <td>
                             <button class="btn btn-info"
-                            onclick="module.edit(${v.moduleId},'${v.moduleName}',${v.duration},${v.status})">Chỉnh sửa</button>
-                            <a href="javascript:void(0)" onclick="module.delete(${v.moduleId},'${v.moduleName}')"
+                            onclick="typeOfRoom.edit(${v.id},'${v.name}',${v.status})">Chỉnh sửa</button>
+                            <a href="javascript:void(0)" onclick="typeOfRoom.delete(${v.id},${v.status})"
                                 class="btn btn-danger"> Xóa
                             </a>
                         </td>
@@ -33,15 +33,16 @@ typeOfRoom.save = function () {
         saveObj.id = parseInt($('#Id').val());
         saveObj.name = $('#Name').val();
         saveObj.status = parseInt($('#Status').val());
+        console.log(status);
         $.ajax({
             url: '/typeofroom/Save',
-            method: 'POST',
+            method: 'PATCH',
             dataType: 'JSON',
             contentType: 'application/json',
             data: JSON.stringify(saveObj),
             success: function (response) {
                 bootbox.alert(response.data.message);
-                if (response.data.moduleId > 0) {
+                if (response.data.id > 0) {
                     $('#addEditTypeOfRoomModal').modal('hide');
                     $("#frmAddEditTypeOfRoom").trigger("reset");
                     typeOfRoom.showData();
@@ -49,6 +50,55 @@ typeOfRoom.save = function () {
             }
         });
     }
+}
+
+typeOfRoom.delete = function (id, status) {
+    bootbox.confirm({
+        message: "Bạn có chắc chắn muốn xóa loại phòng này không <span class='text-danger'>" + v.name + "</span> ?",
+        buttons: {
+            confirm: {
+                label: 'Có',
+                className: 'btn-success'
+            },
+            cancel: {
+                label: 'Không',
+                className: 'btn-danger'
+            }
+        },
+        callback: function (result) {
+            var saveObj = {};
+            saveObj.id = parseInt(id);
+            saveObj.status = 2;
+            console.log(saveObj);
+            if (result) {
+                $.ajax({
+                    url: `/typeofroom/delete`,
+                    method: 'PATCH',
+                    dataType: 'JSON',
+                    contentType: 'application/json',
+                    data: JSON.stringify(saveObj),
+                    success: function (response) {
+                        bootbox.alert(`<h4 class="alert alert-danger">Xóa thành công !!!</h4>`);
+                        if (response.data.id > 0) {
+                            $('#addEditTypeOfRoomModal').modal('hide');
+                            $("#frmAddEditTypeOfRoom").trigger("reset");
+                            typeOfRoom.showData();
+                        }
+                    }
+                });
+            }
+        }
+    });
+}
+
+typeOfRoom.edit = function (id, name, status) {
+    $("#Id").val(id);
+    $("#Name").val(name);
+    $("#Status").val(status);
+    typeOfRoom.initStatus(status);
+    typeOfRoom.openModal();
+    //document.getElementById('st').style.display = 'none';
+    $("#st").hide();
 }
 
 typeOfRoom.initStatus = function (defaultStatus) {
@@ -70,6 +120,7 @@ typeOfRoom.initStatus = function (defaultStatus) {
 
 typeOfRoom.openModal = function () {
     $('#addEditTypeOfRoomModal').modal('show');
+    $("#st").show();
 }
 
 $('#closeButton').on('click', function () {
