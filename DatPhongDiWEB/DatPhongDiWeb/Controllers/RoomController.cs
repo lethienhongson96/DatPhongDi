f48c2f2709a0d6fbda7244974781d256bbef944d
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-11-26 11:35:17 +0700

    hongson-->edit roomcontroller

--- DatPhongDiWEB/DatPhongDiWeb/Controllers/RoomController.cs
+++ DatPhongDiWEB/DatPhongDiWeb/Controllers/RoomController.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DatPhongDiWeb.Models.Room;
+﻿using DatPhongDiWeb.Models.Room;
 using DatPhongDiWeb.Ultilities;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace DatPhongDiWeb.Controllers
 {
