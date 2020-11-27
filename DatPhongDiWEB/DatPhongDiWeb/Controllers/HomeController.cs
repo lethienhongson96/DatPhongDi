b5c0b8c7157dbb1f2ca9031267e7aff6887cb0ef
lethienhongson96 <66103162+lethienhongson96@users.noreply.github.com>
2020-11-27 09:24:50 +0700

    HongSon-->format space in controlerr

--- DatPhongDiWEB/DatPhongDiWeb/Controllers/HomeController.cs
+++ DatPhongDiWEB/DatPhongDiWeb/Controllers/HomeController.cs
@@ -29,7 +29,5 @@ namespace DatPhongDiWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-
     }
 }
