63f74bbe3dd1a32004ebc604cca4f29b9d4cde25
ADMIN <ADMIN@KHOA>
2020-12-09 22:51:49 +0700

    khoa-dev

--- DatPhongDiAPI/DatPhongDi.DAL.Implement/BaseRepository.cs
+++ DatPhongDiAPI/DatPhongDi.DAL.Implement/BaseRepository.cs
@@ -9,7 +9,6 @@ namespace DatPhongDi.DAL.Implement
         public BaseRepository()
         {
             connection = new SqlConnection(@"Data Source=khoa\sqlexpress;Initial Catalog=DatPhongDiDb;Integrated Security=True");
-
         }
     }
 }
